5c47cb7c9e1aceef456bfdcc0efcd68737f76c25
Eric Jones <Ejones@j5rescue.com>
2026-09-07 22:36:32 -0500

    Sitewide themed notices (container width); visible cart Remove buttons

--- woocommerce/cart/cart.php
+++ woocommerce/cart/cart.php
@@ -147,7 +147,7 @@ $cart_count = WC()->cart->get_cart_contents_count();
                         echo apply_filters( // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
                             'woocommerce_cart_item_remove_link',
                             sprintf(
-                                '<a href="%s" class="j5-cart-item__remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">&times;</a>',
+                                '<a href="%s" class="j5-cart-item__remove j5-remove-btn" aria-label="%s" data-product_id="%s" data-product_sku="%s"><span aria-hidden="true">&times;</span> Remove</a>',
                                 esc_url( wc_get_cart_remove_url( $cart_item_key ) ),
                                 /* translators: %s is the product name */
                                 esc_attr( sprintf( __( 'Remove %s from cart', 'woocommerce' ), wp_strip_all_tags( $_product->get_name() ) ) ),
